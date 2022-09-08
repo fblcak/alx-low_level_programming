16931374c8bd6966901a472240516d116c0708ab
fblcak <freddyalabaster@gmail.com>
2022-09-08 20:33:24 +0100

    executed some changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,43 +1,21 @@
 /*
- *
- *  * File: 101-quote.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <unistd.h>
 
-
-
 /**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *
- *   *                followed by a new line, to standard error.
- *
- *    *
- *
- *     * Return: Always 1.
- *
- *      */
-
+ * Return: Always 1.
+ */
 int main(void)
-
 {
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-		write(2,
-
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-
-					      	      59);
-
-
-
-			return (1);
-
+	return (1);
 }
-
-
